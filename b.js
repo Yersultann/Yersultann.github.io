cbe24fe17379d7337da8041f631da35b435136e4
Morwen-S <52970386+Morwen-S@users.noreply.github.com>
2019-10-02 19:22:33 +0600

    Update b.js

--- b.js
+++ b.js
@@ -1,17 +1,39 @@
-var modal = document.getElementById('myModal');
-var btn = document.getElementById('myBtn');
-var span = document.getElementById('close');
+// CAROUSEL
+var slideIndex = 1;
+showSlides(slideIndex);
 
-btn.onclick = function(){
-	modal.style.display = 'block';
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
+// FORM POPUP
+
+var modal = document.getElementById('myForm');
+var modalDiv = document.getElementById('form-div');
+var btn = document.getElementById('order-btn');
+var span = document.getElementsByClassName('close')[0];
+
+btn.onclick = function() {
+  modal.style.display = 'block';
 }
 
 span.onclick = function() {
-	modal.style.display = 'none';
+  modal.style.display = 'none';
 }
 
-window.onclick = function(event) {
-	if(event.target == modal) {
-		modal.style.display = 'none';
-	}
+window.onclick = function() {
+  if (event.target == modalDiv) {
+    modal.style.display = 'none';
+  }
 }
